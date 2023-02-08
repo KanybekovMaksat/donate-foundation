7325cb5f78c9382530886428c9feb7c7b9e84779
Kuma Original <Kuma_Original@gmail.com>
2023-02-08 20:57:50 +0600

    29

--- index.js
+++ index.js
@@ -1,3 +1,28 @@
+const selectSingle = document.querySelector('.__select');
+const selectSingle_title = selectSingle.querySelector('.__select__title');
+const selectSingle_labels = selectSingle.querySelectorAll('.__select__label');
+
+// Toggle menu
+selectSingle_title.addEventListener('click', () => {
+  if ('active' === selectSingle.getAttribute('data-state')) {
+    selectSingle.setAttribute('data-state', '');
+  } else {
+    selectSingle.setAttribute('data-state', 'active');
+  }
+});
+
+// Close when click to option
+for (let i = 0; i < selectSingle_labels.length; i++) {
+  selectSingle_labels[i].addEventListener('click', (evt) => {
+    selectSingle_title.textContent = evt.target.textContent;
+    selectSingle.setAttribute('data-state', '');
+  });
+}
+
+
+
+
+
 const navbarMenu = document.getElementById("menu");
 const burgerMenu = document.getElementById("burger");
 const headerMenu = document.getElementById("header");
@@ -101,7 +126,4 @@ document.getElementById("moreaus__video-player").style.display = "block";
 document.getElementById("moreaus__video-thumbnail_container").style.display = "none";
 player.playVideo();
 });
-
-
-
 
